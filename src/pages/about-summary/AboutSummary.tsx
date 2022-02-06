ae9d1d5f048af93167d9380b4e1c38cb2da43582
Hrishikesh <hrishikeshpaul12@gmail.com>
2022-02-06 17:07:54 -0600

    chore: summary changes

--- src/pages/about-summary/AboutSummary.tsx
+++ src/pages/about-summary/AboutSummary.tsx
@@ -7,18 +7,18 @@ export const AboutSummary: FC = () => {
     const content = useContent(MarkdownFile.AboutSummary);
     return (
         <Box>
-            <Flex pt="8" gap="8" direction={{ base: "column", md: "row" }}>
-                <Box flex="0.3">
+            <Flex pt="8" gap={{ base: 6, md: 6, lg: 12 }} direction={{ base: "column", md: "row" }}>
+                <Box flex="0.35">
                     <Image borderRadius="xl" src={configs.common.mainPicture} w="100%" />
                 </Box>
-                <Box flex="0.6">
+                <Box flex="0.85">
                     <Heading>{configs.common.name}</Heading>
                     <Text fontWeight="bold" opacity="0.5">
                         /ri-shi-kaysh/
                     </Text>
-                    <Content fontSize="lg" pt="4">
-                        {content.aboutSummary}
-                    </Content>
+                    <Box pt="4">
+                        <Content fontSize="lg">{content.aboutSummary}</Content>
+                    </Box>
                 </Box>
             </Flex>
         </Box>

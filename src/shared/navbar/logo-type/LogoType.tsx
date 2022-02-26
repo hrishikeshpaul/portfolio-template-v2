bd7990f1ae07451ad59badee74187fd8665b6585
Hrishikesh <hrishikeshpaul12@gmail.com>
2022-02-26 11:05:31 -0600

    chore: fixed landing

--- src/shared/navbar/logo-type/LogoType.tsx
+++ src/shared/navbar/logo-type/LogoType.tsx
@@ -14,7 +14,7 @@ export const LogoType: FC<Props> = ({ text }) => {
 
     return (
         <Box transition="all 0.2s ease-in-out" _hover={{ cursor: "pointer", color: "primary.600" }}>
-            <Text fontSize={{ base: "3xl", md: "4xl" }} lineHeight="1" fontFamily="Signature" mb="-4">
+            <Text fontSize={{ base: "3xl", md: "4xl" }} lineHeight="1" fontFamily="Signature" mb={{ base: 0, md: -2 }}>
                 {variant}
             </Text>
         </Box>
